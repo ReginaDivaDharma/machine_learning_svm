ac76d47f9d414f057869c3e7a3c6f7ddf48e12c1
unknown <reginadivadharma@gmail.com>
2024-07-04 12:23:47 +0700

    changed ML

--- backend/app.py
+++ backend/app.py
@@ -8,12 +8,12 @@ app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
 # Load the SVM model
-with open('svm_model.pkl', 'rb') as f:
-    svm_model = pickle.load(f)
+with open('logistic_regression.pkl', 'rb') as f:
+    logistic_regression = pickle.load(f)
 
 # Load the TF-IDF vectorizer
-with open('vectorizer.pkl', 'rb') as f:
-    vectorizer = pickle.load(f)
+with open('tfidf_vectorizer.pkl', 'rb') as f:
+    tfidf_vectorizer = pickle.load(f)
 
 class NumpyEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -33,13 +33,13 @@ def predict_sentiment():
         text_data = request.json.get('text')
 
         # Preprocess the text data using the loaded vectorizer
-        text_data_tfidf = vectorizer.transform([text_data])
+        text_data_tfidf = tfidf_vectorizer.transform([text_data])
 
         # Convert sparse matrix to dense matrix
         text_data_dense = text_data_tfidf.toarray()
 
         # Make predictions using the loaded model
-        prediction = svm_model.predict(text_data_dense)[0]
+        prediction = logistic_regression.predict(text_data_dense)[0]
 
         # Map the integer prediction to the corresponding label
         sentiment_label = "positive" if prediction == 1 else "negative"
